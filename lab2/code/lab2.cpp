f13266346dd6379634b5c100feb3fb90663f2ac2
human3562 <31441934+human3562@users.noreply.github.com>
2019-12-14 00:05:44 +0300

    Add files via upload

---
+++ lab2/code/lab2.cpp
@@ -0,0 +1,57 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+double a, b, startX, endX, dx;
+
+double f(double x) {
+	if (x <= a) {
+		return exp(x);
+	}
+	else if (x < b && x > a) {
+		return exp(x) + cos(x);
+	}
+	else if (x >= b) {
+		return tan(x);
+	}
+}
+
+int main()
+{
+	cout << "a = ";
+	cin >> a;
+
+	cout << "b = ";
+	cin >> b;
+
+	cout << "startX = ";
+	cin >> startX;
+	
+	cout << "endX = ";
+	cin >> endX;
+
+	cout << "dx = ";
+	cin >> dx;
+
+	double firstval = f(startX);
+	cout << '('<<startX<<';'<<firstval<<')';
+	double max = firstval;
+	double min = firstval;
+
+	for (double i = startX+dx; i <= endX+dx; i += dx) {
+		double val = f(i);
+		cout << ", (" << i << ';' << val << ')';
+		if (val > max) max = val;
+		if (val < min) min = val;
+	}
+
+
+
+	cout << endl;
+	cout << "max: " << max << endl;
+	cout << "min: " << min << endl;
+
+	return 0;
+}
